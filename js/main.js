f252c183d2ae11345035011c85d080e04091e3af
Thomas Heller <office@thomasheller.net>
2014-03-25 18:49:46 +0100

    Fixed menu bar offset on internal links

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
 	$('#menuToggle').on('click', function(){
 		$('.mainNavigation_container').toggleClass('activeMenu');
 	})
+
+  $('a[href^=#]').on('click', function(evt){
+    evt.preventDefault();
+    $('.mainNavigation_container').removeClass('activeMenu');
+    $('html, body').scrollTop($(evt.target.hash).offset().top - $('.mainNavigation').height());
+  })
 });
